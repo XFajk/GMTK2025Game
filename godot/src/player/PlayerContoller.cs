a13a9b0ca20bb3ffac7892a3aac96259a0641b64
Rudolf Vrbensky <89221002+XFajk@users.noreply.github.com>
2025-07-30 22:59:45 +0200

    Basic Player Controller Done

---
+++ godot/src/player/PlayerContoller.cs
@@ -0,0 +1,41 @@
+using Godot;
+using System;
+
+public partial class PlayerContoller : Camera3D {
+
+    [Export]
+    public float DragScalar = 0.01f;
+
+    [Export]
+    public float ZoomSpeed = 0.01f;
+
+    private Vector2 _previousePosition = Vector2.Zero;
+    private Vector2 _previouseMousePosition = Vector2.Zero;
+
+
+    public override void _Ready() {
+        _previousePosition = new Vector2(Position.X, Position.Y);
+        _previouseMousePosition = GetViewport().GetMousePosition();
+    }
+    public override void _Process(double delta) {
+        if (Input.IsActionPressed("drag")) {
+            Vector2 mousePositionDifference = _previouseMousePosition - GetViewport().GetMousePosition();
+            Vector2 positionOffset = _previousePosition + mousePositionDifference;
+            Position = new Vector3(_previousePosition.X + positionOffset.X * DragScalar * (Fov * 0.01f), _previousePosition.Y - positionOffset.Y * DragScalar * (Fov * 0.01f), Position.Z);
+        } else {
+            _previouseMousePosition = GetViewport().GetMousePosition();
+            _previousePosition = new Vector2(Position.X, Position.Y);
+        }
+
+
+        if (Input.IsActionPressed("zoom_in")) {
+            Fov -= ZoomSpeed;
+        }
+        if (Input.IsActionPressed("zoom_out")) {
+            Fov += ZoomSpeed;
+        }
+
+        Fov = float.Clamp(Fov, 20.0f, 100.0f);
+
+    }
+}
