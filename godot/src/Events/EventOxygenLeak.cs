39a99b2a765e35101451cfded8d969775eb4de88
Geert van Ieperen <ieperen3039@outlook.com>
2025-07-31 15:20:45 +0200

    replace template event with oxygen leak: blocked on float resources

---
+++ godot/src/Events/EventOxygenLeak.cs
@@ -0,0 +1,8 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+
+public partial class EventOxygenLeak : Node3D, IEvent {
+    public void ApplyEffect(List<Connectable> connectables, List<CrewMember> crew) {
+    }
+}
