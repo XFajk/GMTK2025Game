cef3f8f9ee0f19281d07f6ddf2dc5c3f4fa34c7c
Geert van Ieperen <ieperen3039@outlook.com>
2025-07-31 13:51:32 +0200

    Add check for resource loss

--- godot/src/Machines/Resource.cs
+++ godot/src/Machines/Resource.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 public enum Resource {
     /// 1 coolant + 4 hydro
     CoolantHot,
@@ -30,4 +34,41 @@ public class Resources {
         Resource.Humidity or Resource.Oxygen or Resource.CarbonDioxide => true,
         _ => false,
     };
+
+    public static bool IsLossless(IList<InputOutput> receipe) {
+        Dictionary<ResourcePart, int> totals = new();
+
+        // if all resource changes add up to 0, then we lose nothing
+        foreach (InputOutput io in receipe) {
+            var parts = GetParts(io.Resource);
+            foreach (ResourcePart p in parts) {
+                totals[p] += io.QuantityChangeInReceipe;
+            }
+        }
+
+        return totals.Values.All(v => v == 0);
+    }
+
+    // gests
+    private static List<ResourcePart> GetParts(Resource resource) {
+        return resource switch {
+            Resource.CoolantHot or Resource.CoolantCold => [ResourcePart.Coolant, .. Enumerable.Repeat(ResourcePart.Hydro, 4)],
+            Resource.Humidity or Resource.Water => [ResourcePart.Hydro],
+            Resource.Food => [ResourcePart.Hydro, ResourcePart.Carbon],
+            Resource.FluidWaste => [ResourcePart.Hydro],
+            Resource.SolidWaste => [ResourcePart.Carbon],
+            Resource.Oxygen => [ResourcePart.Oxygen],
+            Resource.CarbonDioxide => [ResourcePart.Carbon, ResourcePart.Oxygen],
+            Resource.Disposables or Resource.Garbage => [.. Enumerable.Repeat(ResourcePart.Carbon, 8)],
+            _ => throw new NotImplementedException()
+        };
+    }
+
+    // every resource is composed of a combination of these atoms
+    private enum ResourcePart {
+        Carbon,
+        Hydro,
+        Oxygen,
+        Coolant,
+    }
 }
