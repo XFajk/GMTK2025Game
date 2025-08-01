43e98519debae65feca2c6ec1cc70387fca58a8d
Rudolf Vrbensky <89221002+XFajk@users.noreply.github.com>
2025-08-01 23:55:59 +0200

    Remove debug print statements from plant processing logic

--- godot/src/Machines/Plants.cs
+++ godot/src/Machines/Plants.cs
@@ -25,8 +25,6 @@ public partial class Plants : Machine {
         _plantHealth = Mathf.Clamp(_plantHealth, 0, 2.0f);
         _processingPerSecond = _baseProcessingPerSecond * _plantHealth;
         
-        GD.Print($"Plant {Name} health: {_plantHealth} -> running {_processingPerSecond} times per second");
-        
         // always progess
         _processProgress += _processingPerSecond * (float)deltaTime;
 
@@ -36,7 +34,6 @@ public partial class Plants : Machine {
             // check if all ingredients are present and enough output space available
             foreach (MachineBuffer container in _recipeParts) {
                 if (!CanCycle(container)) {
-                    GD.Print($"Cannot cycle {Name} due to insufficient resources or space. of {container.GetName()}");
                     _processProgress = 0;
                     _plantHealth -= DryingRate; 
                     return;
