14a9467392bb079044a73cd3856ea0c14a5049ac
Geert van Ieperen <ieperen3039@outlook.com>
2025-08-02 19:15:07 +0200

    crew goes to engine when travel mission starts

--- godot/src/Missions/MissionTravel.cs
+++ godot/src/Missions/MissionTravel.cs
@@ -16,16 +16,18 @@ public partial class MissionTravel : Node, IMission {
     [Export(PropertyHint.Range, "0,1,0.05")]
     public float TargetEnginePower;
     public IMission.Properties Properties;
-    
+
     private Ship _ship;
     private ulong _preparationStartTime;
 
     private ulong _startTime;
 
+    private List<Engine> _engines = new();
+
     void IMission.Ready(Ship ship) {
         _ship = ship;
         _preparationStartTime = Time.GetTicksUsec();
-        
+
         Properties = new() {
             Title = "Mission: Reparations",
             Briefing = [
@@ -39,25 +41,30 @@ public partial class MissionTravel : Node, IMission {
                 $"We have arrived! The engine will return to {Engine.DefaultEnginePower * 100}% power."
             ],
         };
+
+        foreach (Machine m in ship.Machines) {
+            if (m is Engine e) {
+                _engines.Add(e);
+                ship.ScheduleCrewTask(new CrewTask() {
+                    Location = e.Position,
+                    Duration = Duration + PreparationTime
+                });
+            }
+        }
     }
 
     public IMission.Properties GetMissionProperties() => Properties;
 
     public void OnStart(Ship ship) {
         _startTime = Time.GetTicksUsec();
-
-        foreach (Machine m in ship.Machines) {
-            if (m is Engine e) {
-                e.EnginePower = TargetEnginePower;
-            }
+        foreach (Engine e in _engines) {
+            e.EnginePower = TargetEnginePower;
         }
     }
 
     public void OnCompletion(Ship ship) {
-        foreach (Machine m in ship.Machines) {
-            if (m is Engine e) {
-                e.EnginePower = Engine.DefaultEnginePower;
-            }
+        foreach (Engine e in _engines) {
+            e.EnginePower = Engine.DefaultEnginePower;
         }
     }
 
