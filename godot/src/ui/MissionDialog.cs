1b38702ae1750343ffc621538120131958da97e3
Rudolf Vrbensky <89221002+XFajk@users.noreply.github.com>
2025-08-02 11:59:19 +0200

    Refactor MissionDialog integration and update UI structure in game_ui.tscn

--- godot/src/ui/MissionDialog.cs
+++ godot/src/ui/MissionDialog.cs
@@ -2,17 +2,18 @@ using Godot;
 using System;
 
 public partial class MissionDialog : AcceptDialog {
-
     private RichTextLabel _description;
+    private Label _missionTitle;
 
     public override void _Ready() {
-        _description = GetNode<RichTextLabel>("Description");
+        _description = GetNode<RichTextLabel>("UI/Description");
+        _missionTitle = GetNode<Label>("UI/MissionTitle");
     }
 
-    public void ShowMission(string title, string description) {
+    public void ShowMission(string windowTitle, string title, string description) {
         Title = title;
         _description.Text = description;
+        _missionTitle.Text = title;
         Popup();
     }
- 
 }
