5b6aa7f71064044da30b6e60c9664e127a15ac3c
Rudolf Vrbensky <89221002+XFajk@users.noreply.github.com>
2025-08-11 10:20:38 +0200

    improved the baked lighting

--- godot/src/misc/Levitation.cs
+++ godot/src/misc/Levitation.cs
@@ -9,11 +9,17 @@ public partial class Levitation : Node3D {
     [Export]
     public float RotationSpeed = 0.0f;
 
+    private Vector3 _initialPosition;
+
+    public override void _Ready() {
+        _initialPosition = Position;
+    }
+
     public override void _Process(double delta) {
         float time = (float)Time.GetTicksMsec() / 1000.0f;
-        float offset = Mathf.Sin(time * LevitationFrequency) * LevitationStrength * 0.001f;
+        float offset = Mathf.Sin(time * LevitationFrequency) * LevitationStrength * 0.02f;
 
-        Position = new Vector3(Position.X, Position.Y + offset, Position.Z);
+        Position = new Vector3(_initialPosition.X, _initialPosition.Y + offset, _initialPosition.Z);
         RotateY(RotationSpeed * (float)delta);
     }
 }
